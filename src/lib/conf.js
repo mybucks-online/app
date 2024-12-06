1c87914a8baffe427f7aa1b49e00e6c74618e9ee
koko37 <engel0088@gmail.com>
2024-12-06 21:02:24 +0800

    add generate/parse url

--- src/lib/conf.js
+++ src/lib/conf.js
@@ -2,6 +2,7 @@ import { Network } from "alchemy-sdk";
 import { Buffer } from "buffer";
 import { ethers } from "ethers";
 import { scrypt } from "scrypt-js";
+import { nanoid } from "nanoid";
 
 const abi = new ethers.AbiCoder();
 
@@ -45,6 +46,23 @@ export const generateHash = async (password, passcode, cb = () => {}) => {
 
 export const getEvmPrivateKey = (h) =>
   ethers.keccak256(abi.encode(["string"], [h]));
+
+export function generateUrl(password, passcode, network) {
+  const merged = Buffer.from(
+    password + "\u0002" + passcode + "\u0002" + network,
+    "utf-8"
+  );
+  const base64Encoded = merged.toString("base64");
+  const padding = nanoid(12);
+  return padding.slice(0, 6) + base64Encoded + padding.slice(6);
+}
+
+export function parseUrl(token) {
+  const payload = token.slice(6, token.length - 6);
+  const base64Decoded = Buffer.from(payload, "base64").toString("utf-8");
+  const [password, passcode, network] = base64Decoded.split("\u0002");
+  return [password, passcode, network];
+}
 
 export const NETWORK = Object.freeze({
   EVM: "evm",
