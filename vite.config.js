73963df9dd69f35a3fa969aa3e59bde8d39a83e1
koko37 <engel0088@gmail.com>
2026-02-09 21:35:24 +0800

    add CSP header

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    assetsInlineLimit: 0,
+  },
   plugins: [
     react(),
     nodePolyfills({
