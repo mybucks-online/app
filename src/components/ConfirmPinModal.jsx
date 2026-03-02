2f3238d1245113d80ce351bcf8cb811dda7ef496
koko37 <engel0088@gmail.com>
2026-03-03 01:43:23 +0800

    yarn format

--- src/components/ConfirmPinModal.jsx
+++ src/components/ConfirmPinModal.jsx
@@ -1,5 +1,6 @@
 import { useContext, useState } from "react";
 import { toast } from "react-toastify";
+import { PIN_MAX_LENGTH, PIN_MIN_LENGTH } from "@mybucks.online/core";
 import styled from "styled-components";
 
 import Button from "@mybucks/components/Button";
@@ -7,7 +8,6 @@ import Input from "@mybucks/components/Input";
 import Modal from "@mybucks/components/Modal";
 import { H3 } from "@mybucks/components/Texts";
 import { StoreContext } from "@mybucks/contexts/Store";
-import { PIN_MAX_LENGTH, PIN_MIN_LENGTH } from "@mybucks.online/core";
 import { PIN_MAX_TRY } from "@mybucks/lib/conf";
 
 const Wrap = styled.div`
