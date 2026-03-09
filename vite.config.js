21627792112e5ed8df736487d19684e777f70dc7
koko37 <engel0088@gmail.com>
2026-03-09 17:21:20 +0800

    update vite.config to fix lint error

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
